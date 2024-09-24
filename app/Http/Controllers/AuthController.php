7a881f731d6a6fc625f4e68403dbb9cccc7ede24
PrometheusDevvv <prometheus.devv@gmail.com>
2024-09-24 08:10:41 -0700

    Finish Register/Login Atuhorisation fucntionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,19 +5,41 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Traits\HttpResponses;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
 
 class AuthController extends Controller
 {
 
     use HttpResponses;
-    public function login(){
+    public function login(LoginUserRequest $request){
+       
+        $request->validated($request->all());
+
+        if(!Auth::attempt($request->only('email', 'password'))){
+            return $this->error("Credentials Don't Match", "vax chemi", 200);
+        }
         return response()->json([
-            'data' => User::all()
-        ]);
+            'token' => "x"
+        ]); 
     }
 
-    public function register(){
-        return response()->json("THis is my register");
+    public function register(StoreUserRequest $request){
+        $request->validated($request->all());
+
+        $request->merge([
+            'password' => Hash::make($request->password)
+        ]);
+
+        $user = User::create($request->all());
+  
+        
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken("API Token For {$user->name}")->plainTextToken
+        ]);
     }
 
     public function logout(){
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -27,10 +28,10 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    // protected $hidden = [
+    //     'password',
+    //     'remember_token',
+    // ];
 
     /**
      * Get the attributes that should be cast.
