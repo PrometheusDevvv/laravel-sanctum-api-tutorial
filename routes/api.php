ce29090f448ae2c7a69b61f4350656531007c46e
PrometheusDevvv <prometheus.devv@gmail.com>
2024-09-24 08:41:27 -0700

    Place logout and tasks routes by middleware auth:sacntum

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,11 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::apiResource('tasks', TaskController::class);
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('tasks', TaskController::class);
+});
+
+
